b6841c3af575f6b8794c60ccf77877279d40d094
prasannapattam <pprasannak@hotmail.com>
2014-04-04 08:27:27 +0530

    Working on notes webapi

--- epos/epos/Controllers/NotesController.cs
+++ epos/epos/Controllers/NotesController.cs
@@ -7,23 +7,33 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using epos.Lib.Repository;
+using System.IO;
+using System.Xml;
+using System.Reflection;
 
 namespace epos.Controllers
 {
     public class NotesController : ApiController
     {
 
-        public AjaxModel<dynamic> Get(PosConstants.NotesType notesType, int patientID, int? examID)
+        //patientid = 1203
+        public AjaxModel<NotesModel> Get(int patientID, int? examID)
         {
-            AjaxModel<dynamic> ajax = new AjaxModel<dynamic>() { Success = true };
+            AjaxModel<NotesModel> ajax = new AjaxModel<NotesModel>() { Success = true };
 
             try
             {
                 //get the last examid when no examid is passed
                 ExamModel exam = PosRepository.ExamGet(patientID, examID);
-                NotesModel notes = new NotesModel() { NotesType = PosConstants.NotesType.New };
+                NotesModel notes = GetNotes(exam);
+                //getting the defaults -------------------------------
+
+                if (exam.ExamDate.Date == DateTime.Today.Date)
+                    notes.NotesType = PosConstants.NotesType.Correct;
+
                 if (exam.SaveID == 1)
                     notes.NotesType = PosConstants.NotesType.Saved;
+
 
             }
             catch (Exception exp)
@@ -34,6 +44,66 @@ namespace epos.Controllers
             }
 
             return ajax;
+        }
+
+        private NotesModel GetNotes(ExamModel exam)
+        {
+            NotesModel notes = new NotesModel() { NotesType = PosConstants.NotesType.New };
+
+            PropertyInfo[] notesFields = notes.GetType().GetProperties();
+            Field value;
+            
+            //looping through the xml and setting the Notes
+            StringReader stringReader = new StringReader(exam.ExamText);
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.CheckCharacters = false;
+            XmlReader reader = XmlReader.Create(stringReader, settings);
+            //XmlTextReader reader = new XmlTextReader(stringReader);
+            //reader.WhitespaceHandling = WhitespaceHandling.None;
+
+            string fieldName = "";
+            string fieldValue = "";
+            string fieldAttr = "";
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        fieldName = reader.Name;
+                        fieldAttr = reader.GetAttribute("CustomColourType");
+                        break;
+                    case XmlNodeType.Text:
+                        fieldValue = reader.Value;
+                        break;
+                    case XmlNodeType.CDATA:
+                        fieldValue = reader.Value;
+                        break;
+                    case XmlNodeType.EndElement:
+                        if(fieldName != "")
+                        {
+                            //SetControlValue(fieldName, fieldValue, fieldAttr);
+
+                            PropertyInfo pi = notesFields.FirstOrDefault(p => p.Name == fieldName);
+
+                            if(pi != null)
+                            {
+                                value = new Field() { Value = fieldValue, ColourType = Convert.ToInt32(fieldAttr) };
+                                pi.SetValue(notes, value);
+                            }
+
+                            fieldName = "";
+                            fieldValue = "";
+                            fieldAttr = "";
+                        }
+                        break;
+                }
+            }
+
+            reader.Close();
+            stringReader.Close();
+            stringReader.Dispose();
+
+            return notes;
         }
     }
 
--- epos/epos/Models/NotesModel.cs
+++ epos/epos/Models/NotesModel.cs
@@ -9,21 +9,21 @@ namespace epos.Models
 {
     public class NotesModel
     {
-        //default fields
+        //defaults
         public Field hdnExamDefaultID { get; set; }
         public Field DefaultName { get; set; }
         public Field AgeStart { get; set; }
         public Field AgeEnd { get; set; }
         public Field PrematureBirth { get; set; }
-        public Field DoctorID { get; set; }
-        public List<SelectListItem> Doctors { get; set; }
+        public Field DoctorList { get; set; }
 
-        //patient fields
+        //Hidden fields
         public Field hdnPatientID { get; set; }
         public Field hdnExamID { get; set; }
         public Field hdnNoteType { get; set; }
         public Field hdnColourType { get; set; }
-        public Field hdnDefaultInd { get; set; }
+
+        //Patient
         public Field Greeting { get; set; }
         public Field FirstName { get; set; }
         public Field MiddleName { get; set; }
@@ -35,17 +35,14 @@ namespace epos.Models
         public Field Sex { get; set; }
         public Field BoyGirlAdult { get; set; }
         public Field Premature { get; set; }
-        public Field ExamDate { get; set; }
         public Field User { get; set; }
         public Field HxFrom { get; set; }
-        public Field ddlHxFrom { get; set; }
-        public Field tbHxOther { get; set; }
         public Field Refd { get; set; }
         public Field RefDoctor { get; set; }
         public Field Allergies { get; set; }
         public Field Occupation { get; set; }
-
-        //Compliant Tab
+        
+        //Compliant tab
         public Field Compliant { get; set; }
         public Field SubjectiveHistory { get; set; }
         public Field Mentation1 { get; set; }
@@ -74,8 +71,8 @@ namespace epos.Models
         public Field PHM4 { get; set; }
         public Field BirthHist1 { get; set; }
         public Field BirthHist2 { get; set; }
-        public Field BirthHist3 { get; set; }
         public Field GA { get; set; }
+        public Field BirthHist3 { get; set; }
         public Field PCA { get; set; }
         public Field BirthWt { get; set; }
         public Field DevelopHist1 { get; set; }
@@ -89,7 +86,7 @@ namespace epos.Models
         public Field FH5 { get; set; }
         public Field FH6 { get; set; }
 
-        //Visual
+        //Acuity
         public Field VisualAcuity { get; set; }
         public Field Binocsc1 { get; set; }
         public Field Binocsc2 { get; set; }
@@ -126,11 +123,196 @@ namespace epos.Models
         public Field CycRfxOS { get; set; }
         public Field CycVSOS1 { get; set; }
         public Field CycVSOS2 { get; set; }
-        public Field Greeting { get; set; } //prior exam
-        public Field Greeting { get; set; }
-        public Field Greeting { get; set; }
-        public Field Greeting { get; set; }
+        public Field LastManRfxOD1 { get; set; }
+        public Field LastManRfxOD2 { get; set; }
+        public Field LastManVAOD1 { get; set; }
+        public Field LastManVAOD2 { get; set; }
+        public Field LastCycRfxOD { get; set; }
+        public Field LastCycVAOD3 { get; set; }
+        public Field LastCycVAOD4 { get; set; }
+        public Field LastManRfxOS1 { get; set; }
+        public Field LastManRfxOS2 { get; set; }
+        public Field LastManVSOS1 { get; set; }
+        public Field LastManVSOS2 { get; set; }
+        public Field LastCycRfxOS { get; set; }
+        public Field LastCycVSOS1 { get; set; }
+        public Field LastCycVSOS2 { get; set; }
+        public Field RxOD1 { get; set; }
+        public Field RxOD2 { get; set; }
+        public Field PopulateRxGiven { get; set; }
+        public Field CTLRxOD1 { get; set; }
+        public Field CTLRxOD2 { get; set; }
+        public Field CTLRxOD3 { get; set; }
+        public Field PopulateCtlRx { get; set; }
+        public Field RXOS1 { get; set; }
+        public Field RXOS2 { get; set; }
+        public Field RXOS3 { get; set; }
+        public Field RXOS4 { get; set; }
+        public Field CTLRxOS1 { get; set; }
+        public Field CTLRxOS2 { get; set; }
+        public Field CTLRxOS3 { get; set; }
+        public Field Confront1 { get; set; }
+        public Field Confront2 { get; set; }
+        public Field Confront3 { get; set; }
+        public Field PupilOD1 { get; set; }
+        public Field PupilOD2 { get; set; }
+        public Field PupilOS1 { get; set; }
+        public Field PupilOS2 { get; set; }
+        public Field Pupil { get; set; }
 
+        //Ocular Motility
+        public Field OcularMotility6 { get; set; }
+        public Field OcularMotility1 { get; set; }
+        public Field OcularMotility5 { get; set; }
+        public Field OcularMotility2 { get; set; }
+        public Field OcularMotility4 { get; set; }
+        public Field OMDefaults { get; set; }
+        public Field OcMot1a { get; set; }
+        public Field OcMot1b { get; set; }
+        public Field OcMot2a { get; set; }
+        public Field OcMot2b { get; set; }
+        public Field OcMot3a { get; set; }
+        public Field OcMot3b { get; set; }
+        public Field OcMot4a { get; set; }
+        public Field OcMot4b { get; set; }
+        public Field OcMot5a { get; set; }
+        public Field OcMot5b { get; set; }
+        public Field OcMot6a { get; set; }
+        public Field OcMot6b { get; set; }
+        public Field OcMot7a { get; set; }
+        public Field OcMot7b { get; set; }
+        public Field OcMot8a { get; set; }
+        public Field OcMot8b { get; set; }
+        public Field OcMot9a { get; set; }
+        public Field OcMot9b { get; set; }
+        public Field HeadTiltRight1 { get; set; }
+        public Field HeadTiltRight2 { get; set; }
+        public Field HeadTiltLeft1 { get; set; }
+        public Field HeadTiltLeft2 { get; set; }
+        public Field HeadPosition1 { get; set; }
+        public Field HeadPosition2 { get; set; }
+        public Field DMR2 { get; set; }
+        public Field Preference { get; set; }
+        public Field OcularVersions1 { get; set; }
+        public Field OcularVersions11 { get; set; }
+        public Field OcularVersions2 { get; set; }
+        public Field OcularVersions3 { get; set; }
+        public Field OcularVersions31 { get; set; }
+        public Field OcularVersions4 { get; set; }
+        public Field OcularVersions5 { get; set; }
+        public Field Nystagmus1 { get; set; }
+        public Field Nystagmus2 { get; set; }
+        public Field OcularVersionOD1 { get; set; }
+        public Field OcularVersionOD2 { get; set; }
+        public Field OcularVersionOD3 { get; set; }
+        public Field OcularVersionOD4 { get; set; }
+        public Field OcularVersionOD5 { get; set; }
+        public Field OcularVersionOD6 { get; set; }
+        public Field OcularVersionOS1 { get; set; }
+        public Field OcularVersionOS2 { get; set; }
+        public Field OcularVersionOS3 { get; set; }
+        public Field OcularVersionOS4 { get; set; }
+        public Field OcularVersionOS5 { get; set; }
+        public Field OcularVersionOS6 { get; set; }
+        public Field FusAmpBO { get; set; }
+        public Field FusAmpBI { get; set; }
+        public Field FusAmpBU { get; set; }
+        public Field FusAmpBD { get; set; }
+        public Field Convergence { get; set; }
+        public Field Accommodation { get; set; }
+        public Field LVRR1 { get; set; }
+        public Field LVRR2 { get; set; }
+        public Field LVRL1 { get; set; }
+        public Field LVRL2 { get; set; }
+        public Field Binocularity1 { get; set; }
+        public Field Binocularity2 { get; set; }
+        public Field Binocularity3 { get; set; }
+        public Field Binocularity4 { get; set; }
+        public Field W4DNear1 { get; set; }
+        public Field W4DNear2 { get; set; }
+        public Field W4DDistance1 { get; set; }
+        public Field W4DDistance2 { get; set; }
+        public Field Stereo1 { get; set; }
+        public Field Stereo2 { get; set; }
+
+        //Anteior Segnment
+        public Field AnteriorSegment { get; set; }
+        public Field SLE { get; set; }
+        public Field PenLight { get; set; }
+        public Field AllWNL { get; set; }
+        public Field LidLashLacrimal1 { get; set; }
+        public Field LidLashLacrimal2 { get; set; }
+        public Field LidLashLacrimal3 { get; set; }
+        public Field LidLashLacrimal4 { get; set; }
+        public Field LidLashLacrimal5 { get; set; }
+        public Field Exophthalmometry { get; set; }
+        public Field ExophthalmometryOD { get; set; }
+        public Field ExophthalmometryOS { get; set; }
+        public Field ODPF { get; set; }
+        public Field ODMRD1 { get; set; }
+        public Field ODMRD2 { get; set; }
+        public Field ODLevatorFxn1 { get; set; }
+        public Field ODLevatorFxn2 { get; set; }
+        public Field OSPF { get; set; }
+        public Field OSMRD1 { get; set; }
+        public Field OSMRD2 { get; set; }
+        public Field OSLevatorFxn1 { get; set; }
+        public Field OSLevatorFxn2 { get; set; }
+        public Field ConjSclera1 { get; set; }
+        public Field ConjSclera2 { get; set; }
+        public Field ConjSclera3 { get; set; }
+        public Field ConjSclera4 { get; set; }
+        public Field ConjSclera5 { get; set; }
+        public Field Cornea1 { get; set; }
+        public Field Cornea2 { get; set; }
+        public Field Cornea3 { get; set; }
+        public Field Cornea4 { get; set; }
+        public Field Cornea5 { get; set; }
+        public Field AntChamber1 { get; set; }
+        public Field AntChamber2 { get; set; }
+        public Field AntChamber3 { get; set; }
+        public Field AntChamber4 { get; set; }
+        public Field AntChamber5 { get; set; }
+        public Field Iris1 { get; set; }
+        public Field Iris2 { get; set; }
+        public Field Iris3 { get; set; }
+        public Field Iris4 { get; set; }
+        public Field Iris5 { get; set; }
+        public Field Lens1 { get; set; }
+        public Field Lens2 { get; set; }
+        public Field Lens3 { get; set; }
+        public Field Lens4 { get; set; }
+        public Field Lens5 { get; set; }
+        public Field Tono1 { get; set; }
+        public Field Tono2 { get; set; }
+        public Field Tono3 { get; set; }
+        public Field Tono4 { get; set; }
+        public Field Dilate1 { get; set; }
+        public Field Dilate2 { get; set; }
+        public Field Dilate3 { get; set; }
+        public Field Fundus1 { get; set; }
+        public Field Fundus2 { get; set; }
+        public Field RetinaOU { get; set; }
+        public Field RetOD { get; set; }
+        public Field RetOS { get; set; }
+        public Field MaculaOD { get; set; }
+        public Field MaculaOS { get; set; }
+
+        //Summary
+        public Field Summary { get; set; }
+        public Field Discussed { get; set; }
+        public Field Advised { get; set; }
+        public Field FollowUp1 { get; set; }
+        public Field FollowUp2 { get; set; }
+        public Field FollowUp3 { get; set; }
+        public Field FollowUp4 { get; set; }
+        public Field CopyTo { get; set; }
+        public Field cbPrintQueue { get; set; }
+        public Field ExamNoteTo { get; set; }
+        public Field Notes { get; set; }
+        public Field patientID { get; set; }
+        public Field hdnPageTitle { get; set; }
+        public Field hdnTimeOut { get; set; }
 
         public PosConstants.NotesType NotesType { get; set; }
     }
