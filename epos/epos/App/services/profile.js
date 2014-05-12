0cf005b917bb069dedf888e4fbd502b516afdbcb
Prasanna Pattam <pprasannak@hotmail.com>
2014-05-12 20:07:49 +0530

    Changed the default image location

--- epos/epos/App/services/profile.js
+++ epos/epos/App/services/profile.js
@@ -3,7 +3,7 @@
     var firstName = ko.observable();
     var lastName = ko.observable();
     var userName = ko.observable('prasanna');
-    var photoUrl = ko.observable('/data/prasanna.jpg');
+    var photoUrl = ko.observable('/pos/data/prasanna.jpg');
     var isAuthenticated = ko.observable(utility.isAuthenticated);
 
 
